2408891f865d7190cb9dff8b2745227f3e4c04b3
Ian Cook <icook@cloudera.com>
2019-09-19 12:57:36 -0400

    Add tests

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -58,3 +58,124 @@ test_that("Full example #3 returns expected result", {
       head(5)
   )
 })
+
+test_that("Full example #4 returns expected result", {
+  skip_if_not(exists("inventory"), message = "Test data not loaded")
+  expect_equal(
+    query(
+      "SELECT shop, game, price,
+           CASE
+             WHEN CAST(price AS double) IS NULL THEN CAST('missing price' AS string)
+             WHEN price > 10 THEN 'high price'
+             ELSE CAST('low price' AS string)
+           END AS price_category
+         FROM inventory;"
+    ),
+    inventory %>%
+      transmute(
+        shop, game, price,
+        price_category = case_when(
+          is.na(as.numeric(price)) ~ as.character("missing price"),
+          price > 10 ~ "high price",
+          TRUE ~ as.character("low price")
+        )
+      )
+  )
+})
+
+test_that("Full example #5 returns expected result", {
+  skip_if_not(exists("offices"), message = "Test data not loaded")
+  expect_equal(
+    query(
+      "SELECT upper(city) AS city,
+           CASE state_province
+             WHEN \"Illinois\" THEN \"IL\"
+           END AS st,
+           upper(country) AS country
+         FROM offices
+         WHERE country = \"us\""
+    ),
+    offices %>%
+      filter(country == "us") %>%
+      transmute(
+        city = toupper(city),
+        st = case_when(state_province == "Illinois" ~ "IL"),
+        country = toupper(country)
+      )
+  )
+})
+
+test_that("Full example #6 returns expected result", {
+  skip("currently unsupported")
+  skip_if_not(exists("offices"), message = "Test data not loaded")
+  patterns <- eval(str2lang(paste0("list(",
+    paste0("expr(state_province == '", state.name, "' ~ '", state.abb, "'", collapse = ",\n"),
+    "))")))
+  expect_equal(
+    query(
+      "SELECT upper(city) AS city,
+           CASE state_province
+             WHEN \"Alabama\" THEN \"AL\"
+             WHEN \"Alaska\" THEN \"AK\"
+             WHEN \"Arizona\" THEN \"AZ\"
+             WHEN \"Arkansas\" THEN \"AR\"
+             WHEN \"California\" THEN \"CA\"
+             WHEN \"Colorado\" THEN \"CO\"
+             WHEN \"Connecticut\" THEN \"CT\"
+             WHEN \"Delaware\" THEN \"DE\"
+             WHEN \"Florida\" THEN \"FL\"
+             WHEN \"Georgia\" THEN \"GA\"
+             WHEN \"Hawaii\" THEN \"HI\"
+             WHEN \"Idaho\" THEN \"ID\"
+             WHEN \"Illinois\" THEN \"IL\"
+             WHEN \"Indiana\" THEN \"IN\"
+             WHEN \"Iowa\" THEN \"IA\"
+             WHEN \"Kansas\" THEN \"KS\"
+             WHEN \"Kentucky\" THEN \"KY\"
+             WHEN \"Louisiana\" THEN \"LA\"
+             WHEN \"Maine\" THEN \"ME\"
+             WHEN \"Maryland\" THEN \"MD\"
+             WHEN \"Massachusetts\" THEN \"MA\"
+             WHEN \"Michigan\" THEN \"MI\"
+             WHEN \"Minnesota\" THEN \"MN\"
+             WHEN \"Mississippi\" THEN \"MS\"
+             WHEN \"Missouri\" THEN \"MO\"
+             WHEN \"Montana\" THEN \"MT\"
+             WHEN \"Nebraska\" THEN \"NE\"
+             WHEN \"Nevada\" THEN \"NV\"
+             WHEN \"New Hampshire\" THEN \"NH\"
+             WHEN \"New Jersey\" THEN \"NJ\"
+             WHEN \"New Mexico\" THEN \"NM\"
+             WHEN \"New York\" THEN \"NY\"
+             WHEN \"North Carolina\" THEN \"NC\"
+             WHEN \"North Dakota\" THEN \"ND\"
+             WHEN \"Ohio\" THEN \"OH\"
+             WHEN \"Oklahoma\" THEN \"OK\"
+             WHEN \"Oregon\" THEN \"OR\"
+             WHEN \"Pennsylvania\" THEN \"PA\"
+             WHEN \"Rhode Island\" THEN \"RI\"
+             WHEN \"South Carolina\" THEN \"SC\"
+             WHEN \"South Dakota\" THEN \"SD\"
+             WHEN \"Tennessee\" THEN \"TN\"
+             WHEN \"Texas\" THEN \"TX\"
+             WHEN \"Utah\" THEN \"UT\"
+             WHEN \"Vermont\" THEN \"VT\"
+             WHEN \"Virginia\" THEN \"VA\"
+             WHEN \"Washington\" THEN \"WA\"
+             WHEN \"West Virginia\" THEN \"WV\"
+             WHEN \"Wisconsin\" THEN \"WI\"
+             WHEN \"Wyoming\" THEN \"WY\"
+           END AS st,
+           upper(country) AS country
+         FROM offices
+         WHERE country = \"us\""
+    ),
+    offices %>%
+      filter(country == "us") %>%
+      transmute(
+        city = toupper(city),
+        st = case_when(!!!patterns),
+        country = toupper(country)
+      )
+  )
+})
