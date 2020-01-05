4a0fa36537ddc747de0a556115a152d96054e41a
Ian Cook <icook@cloudera.com>
2020-01-05 00:37:54 -0500

    Document show_dplyr() function (#8)

--- R/show_dplyr.R
+++ R/show_dplyr.R
@@ -12,6 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#' Show dplyr code equivalent to a SQL query
+#'
+#' @description \code{show_dplyr} takes a SQL \code{SELECT} statement and prints
+#'   equivalent dplyr code
+#'
+#' @param data a data frame or data frame-like object (optional)
+#' @param sql a character string containing a SQL \code{SELECT} statement
+#' @details For more details, see \code{\link{query}}. Instead of running the
+#'   dplyr code like \code{query} does, \code{show_dplyr} prints the dplyr code.
+#' @examples
+#' library(dplyr)
+#' library(nycflights13)
+#'
+#' query <- "SELECT origin, dest,
+#'     COUNT(flight) AS num_flts,
+#'     round(AVG(distance)) AS dist,
+#'     round(AVG(arr_delay)) AS avg_delay
+#'   FROM flights
+#'   WHERE distance BETWEEN 200 AND 300
+#'     AND air_time IS NOT NULL
+#'   GROUP BY origin, dest
+#'   HAVING num_flts > 5000
+#'   ORDER BY num_flts DESC, avg_delay DESC
+#'   LIMIT 100;"
+#'
+#' show_dplyr(query)
+#' @seealso \code{\link{query}}
 #' @export
 show_dplyr <- function(data, sql) {
   query_(data, sql, FALSE)
