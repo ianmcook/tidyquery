6c34f0c45b1157469af498c150f5227c6713341f
Ian Cook <icook@cloudera.com>
2020-01-05 01:49:19 -0500

    Diagnose test issue

--- tests/testthat/test-tbl_sql.R
+++ tests/testthat/test-tbl_sql.R
@@ -1,5 +1,4 @@
 test_that("Simple SELECT example query #1 returns expected result on tbl_sql", {
-  skip_if_not(exists("iris_db"), message = "Test data not loaded")
   expect_equal(
     query("SELECT Species, COUNT(*) AS n FROM iris_db GROUP BY Species") %>%
       collect(),
