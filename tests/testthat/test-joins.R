f8301b44135cb6fc13f25b53b4eda6d9aa7497b4
Ian Cook <icook@cloudera.com>
2020-01-14 17:58:20 -0500

    Add tests for check for misqualified column references

--- tests/testthat/test-joins.R
+++ tests/testthat/test-joins.R
@@ -34,3 +34,17 @@ test_that("Join fails when column names have suffixes matching table names or al
     "Names"
   )
 })
+
+test_that("Join fails on misqualified column reference example #1", {
+  expect_error(
+    query("SELECT concat_ws(' ', 'Now boarding', a.name, 'flight', CAST(a.flight AS string)) FROM flights f JOIN airlines a USING (carrier)"),
+    "a.flight"
+  )
+})
+
+test_that("Join fails on misqualified column reference example #2", {
+  expect_error(
+    query("SELECT concat_ws(' ', 'Now boarding', airlines.name, 'flight', CAST(airlines.flight AS string)) FROM flights f JOIN airlines a USING (carrier)"),
+    "airlines.flight"
+  )
+})
