8497f5c09fcd8d7d3726a2eaa1258e28d21bfecd
Ian Cook <ianmcook@gmail.com>
2021-02-07 13:11:13 -0500

    Fix line breaks

--- R/query.R
+++ R/query.R
@@ -363,7 +363,7 @@ query_ <- function(data, sql, query = TRUE) {
       final_select_list[names(missing_exprs)] <-
         lapply(names(final_select_list[names(missing_exprs)]), as.name)
     }
- 
+
     if (length(aliases) > 0) {
       out <- out %>% verb(mutate, !!!aliases)
     }
